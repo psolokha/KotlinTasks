901fc11d79edcff2e4a9b8c87d05541b4589f70f
Pavel Solokha <pavel.solokha@gmail.com>
2020-04-21 10:48:12 +0300

    Task13

--- src/Tasks3.kt
+++ src/Tasks3.kt
@@ -1,5 +1,6 @@
 fun main(args: Array<String>) {
     task12()
+    task13()
 }
 
 fun task12() {
@@ -10,4 +11,13 @@ fun task12() {
 fun is_leap(year: Int) : String {
     return  if (year%4 == 0) "leap"
     else "common"
+}
+
+fun task13() {
+    val num = staff("Jack", "Jim", "Johnnie", "William")
+    println("staff counter: $num")
+}
+
+fun staff(vararg name:String): Int {
+    return name.size
 }
