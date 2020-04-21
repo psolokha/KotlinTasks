9a3d51162e729eaa86b00c63117644751d2c1542
Pavel Solokha <pavel.solokha@gmail.com>
2020-04-21 11:23:07 +0300

    Task16

--- src/Tasks3.kt
+++ src/Tasks3.kt
@@ -1,8 +1,11 @@
+import kotlin.math.sqrt
+
 fun main(args: Array<String>) {
     task12()
     task13()
     task14()
     task15()
+    task16()
 }
 
 fun task12() {
@@ -36,4 +39,9 @@ fun task15() {
     val arrStr : Array<String> = arrayOf("1","2","3","4")
     printStr(arrStr)
 
+}
+
+fun task16() {
+    fun Double.sqrtX(): Double {return sqrt(this)}
+    println(9.0.sqrtX())
 }
