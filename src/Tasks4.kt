032d9d839971739d75caa77d4007cdef149b7f8a
Pavel Solokha <pavel.solokha@gmail.com>
2020-04-24 09:35:41 +0300

    Task23

--- src/Tasks4.kt
+++ src/Tasks4.kt
@@ -5,4 +5,16 @@ fun main(args: Array<String>) {
     task20()
     task21()
     task22()
+    task23()
+}
+
+fun task23() {
+    val myCar = Car("Nissan", "White", "A661AA50")
+    println(myCar.toString())
+}
+
+data class Car(val brand: String, val color: String, val num: String) {
+    override fun toString(): String {
+        return "Car brand: $brand, color: $color, number: $num"
+    }
 }
